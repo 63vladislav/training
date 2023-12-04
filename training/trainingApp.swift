a4be7619eab3c70ea862f55b6da8446eb782b320
chichafon <chichafonchichafon@gmail.com>
2023-12-04 17:20:37 +0400

    Initial Commit

---
+++ training/trainingApp.swift
@@ -0,0 +1,17 @@
+//
+//  trainingApp.swift
+//  training
+//
+//  Created by 111 on 04.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct trainingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
